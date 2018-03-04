a922503736b622289ef9d688567118de07938558
duxianwei520 <duxianwei520@126.com>
2018-03-04 12:28:49 +0800

    肚皮 返回值是-1的时候跳出登录，其他的不做处理

--- app/utils/index.js
+++ app/utils/index.js
@@ -45,9 +45,12 @@ export const createAjaxAction = (api, startAction, endAction) => (data, cb, reje
             message.error(respon.msg)
           }
           break
+        case -1:
+          logOut()
+          break
         default:
           console.log('status的返回值不是0或1')
-          logOut()
+          // logOut()
         }
       })
       .catch(catchError) // eslint-disable-line no-use-before-define
